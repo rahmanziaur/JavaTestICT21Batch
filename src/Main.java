f548fb79cb690d74ab6cf6837d1847d08f2542a0
ziaqut <154807487+ziaqut@users.noreply.github.com>
2026-06-08 10:11:33 +0600

    classess added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
-public class MainClass {
-
-    public static double getArea() {
-        Circle circle = new Circle();
-        circle.setRadius(5);
-        return Math.PI * circle.getRadius() * circle.getRadius();
-    }
+// Main Class
+public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Area of Circle:"+getArea());
+        /*
+         * Parent reference, Child object
+         * Demonstrates Runtime Polymorphism
+         */
+        Car car = new ToyotaPremio(
+                "White",
+                "Premio G Superior",
+                "Japan",
+                2023,
+                "Sedan"
+        );
 
+        System.out.println("\n--- Method Calls ---");
+
+        // Calls overridden method of ToyotaPremio
+        car.carType();
+
+        // Calls overridden method of ToyotaPremio
+        car.status();
     }
 }
