6f3562353eea5e34096181a3ed955c3de52dfeb7
Ziaur Rahman, PhD <zia@iut-dhaka.edu>
2026-06-08 10:06:14 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-public class Main {
+public class MainClass {
+
+    public static double getArea() {
+        Circle circle = new Circle();
+        circle.setRadius(5);
+        return Math.PI * circle.getRadius() * circle.getRadius();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Area of Circle:"+getArea());
+
+    }
 }
